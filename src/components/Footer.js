1f9d1013fb7a632ea12acb969e6f3e37ec4cb537
jerryowusu <jerryowusu50@gmail.com>
2024-01-30 18:07:28 +0000

    Remove javascript:void error

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,14 @@ const Footer = () => {
             <div className="px-4 py-8 sm:px-12">
                 <div className="flex flex-wrap items-center justify-between">
                     <div className="w-full mb-6 text-center md:w-auto md:text-left md:mb-0">
-                        <a href="javascript:void(0)" className="text-2xl font-extrabold text-white hover:text-gray-500">Jerry</a>
+                        <a href="#" className="text-2xl font-extrabold text-white hover:text-gray-500">Jerry</a>
                     </div>
                     <div className="w-full text-center md:w-auto">
                         <ul className="flex flex-wrap items-center justify-center space-x-6 gap-y-2 md:justify-end">
-                            <li><a href="javascript:void(0)" className="text-base text-white hover:text-gray-900">Home</a></li>
-                            <li><a href="javascript:void(0)" className="text-base text-white hover:text-gray-900">About</a></li>
-                            <li><a href="javascript:void(0)" className="text-base text-white hover:text-gray-900">Services</a></li>
-                            <li><a href="javascript:void(0)" className="text-base text-white hover:text-gray-900">Contact</a></li>
+                            <li><a href="#" className="text-base text-white hover:text-gray-900">Home</a></li>
+                            <li><a href="#" className="text-base text-white hover:text-gray-900">About</a></li>
+                            <li><a href="#" className="text-base text-white hover:text-gray-900">Services</a></li>
+                            <li><a href="#" className="text-base text-white hover:text-gray-900">Contact</a></li>
                         </ul>
                     </div>
                 </div>
